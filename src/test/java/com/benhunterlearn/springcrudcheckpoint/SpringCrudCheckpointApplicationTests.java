56521c1ea685e0bc426af67991a8c92c7413efcc
Ben Hunter <learntocode@benhunter.me>
2021-09-27 14:16:34 -0500

    Initial

---
+++ src/test/java/com/benhunterlearn/springcrudcheckpoint/SpringCrudCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudcheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
